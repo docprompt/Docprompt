59ff52a2a7affcbccfd844ae003d64f514a19bf4
Frank Colson <frankie.colson@gmail.com>
2023-11-02 14:15:13 -0600

    Optimize date matching

--- docprompt/textual/date_extraction.py
+++ docprompt/textual/date_extraction.py
@@ -11,21 +11,13 @@ end_pattern = r"(?:[;,.]?\s|$)"
 
 DEFAULT_PATTERNS = [
     # Numerical dates
-    r"(?:^|\s)\d{1,2}/\d{1,2}/\d{4}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{1,2}/\d{1,2}/\d{2}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{4}/\d{1,2}/\d{1,2}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{1,2}-\d{1,2}-\d{4}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{1,2}-\d{1,2}-\d{2}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{4}-\d{1,2}-\d{1,2}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{4}\.\d{1,2}\.\d{1,2}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{1,2}\.\d{1,2}\.\d{4}(?:[;,.]?\s|$)",
-    r"(?:^|\s)\d{1,2}\.\d{1,2}\.\d{2}(?:[;,.]?\s|$)",
+    r"(?:^|\s)(\d{4}[-/.]\d{1,2}[-/.]\d{1,2}|\d{1,2}[-/.]\d{1,2}[-/.]\d{2,4})(?:[;,.]?\s|$)",
     # Month name (full and abbreviated) followed by day and year
-    r"(?:^|\s)(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d{1,2}(?:st|nd|rd|th)?,?\s+\d{4}(?:[;,.]?\s|$)",  # Added ordinal suffixes
+    r"(?:^|\s)((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d{1,2}(?:st|nd|rd|th)?,?\s+\d{4})(?:[;,.]?\s|$)",  # Added ordinal suffixes
     # Day followed by month name (full and abbreviated) and year
-    r"(?:^|\s)\d{1,2}(?:st|nd|rd|th)?\s+(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d{4}(?:[;,.]?\s|$)",  # Added ordinal suffixes
+    r"(?:^|\s)(\d{1,2}(?:st|nd|rd|th)?\s+(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d{4})(?:[;,.]?\s|$)",  # Added ordinal suffixes
     # Year followed by month name (full and abbreviated) and day
-    r"(?:^|\s)\d{4}\s+(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d{1,2}(?:[;,.]?\s|$)",
+    r"(?:^|\s)(\d{4}\s+(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+\d{1,2})(?:[;,.]?\s|$)",
 ]
 
 
@@ -40,6 +32,23 @@ def register_date_pattern(pattern: str):
 
 def get_date_pattern_regexes():
     return [re.compile(x, re.IGNORECASE) for x in DEFAULT_PATTERNS]
+
+
+def get_date_matches_for_text(
+    text: str, unique: bool = False, patterns: Optional[list[re.Pattern]] = None
+) -> list[re.Match]:
+    results = []
+
+    patterns = patterns or get_date_pattern_regexes()
+
+    for regex in patterns:
+        for match in regex.finditer(text):
+            results.append(match)
+
+    if unique:
+        return list(set(results))
+
+    return results
 
 
 def get_date_strings_for_text(
