0f1d9ceed15cd94caa03e8fd69b069fce291d99f
Frank Colson <frankie.colson@gmail.com>
2024-03-18 10:16:56 -0600

    Bump version

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,4 +1,6 @@
 from docprompt import load_document
+from PIL import Image
+import io
 
 from .fixtures import PDF_FIXTURES
 
@@ -8,3 +10,11 @@ def test_load_document():
         doc = load_document(fixture.get_full_path())
         assert doc.page_count == fixture.page_count
         assert doc.document_hash == fixture.file_hash
+
+
+def test_rasterize():
+    # Fow now just test PIL can open the image
+    for fixture in PDF_FIXTURES:
+        doc = load_document(fixture.get_full_path())
+        img_bytes = doc.rasterize_page(1)
+        Image.open(io.BytesIO(img_bytes))
