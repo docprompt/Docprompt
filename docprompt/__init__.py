bb569132e70493f949c7988ef40029c0a6ac3b23
Frank Colson <frankie.colson@gmail.com>
2024-05-05 19:13:45 -0600

    Bump version, add to test matrix

--- docprompt/__init__.py
+++ docprompt/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Frankie Colson"""
 __email__ = "frank@pageleaf.io"
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from docprompt.schema.document import Document, PdfDocument  # noqa
 from docprompt.schema.layout import NormBBox, TextBlock  # noqa
