ff21373fe4ec87b3e1225b9abc67c4b103ce7052
Frank Colson <frankie.colson@gmail.com>
2024-09-24 22:54:00 -0600

    Bump version

--- docprompt/__init__.py
+++ docprompt/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Frankie Colson"""
 __email__ = "frank@pageleaf.io"
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 
 from docprompt.rasterize import ProviderResizeRatios
 from docprompt.schema.document import Document, PdfDocument  # noqa
